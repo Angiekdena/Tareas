4be5fb87712b5d69ef744c4e06fad0a0086a069b
Angie Cadena <ltejedor@outlook.com>
2023-06-12 16:45:30 -0500

    Tarea Funciones

--- Funciones/script.js
+++ Funciones/script.js
@@ -6,3 +6,32 @@
 // De rojo pasa a Verde.
 // De amarillo puede pasar a Rojo.
 // De Verde pasa a Amarillo.
+
+const arreglo = ["#009900", "#ffd700", "#ff0000"];
+let actual = 0;
+
+function cambiarColor(inicial) {
+  let color = arreglo[0];
+  let elemento = document.getElementsByClassName("verde");
+
+  if (inicial == 0) {
+    actual = 1;
+    document.getElementsByClassName("rojo")[0].style.backgroundColor = "rgb(255, 0, 0, 0.3)";
+  } else if (inicial == 1) {
+    actual = 2;
+    elemento = document.getElementsByClassName("amarillo");
+    document.getElementsByClassName("verde")[0].style.backgroundColor = "rgb(0, 153, 0, 0.3)";
+  } else {
+    actual = 0;
+    elemento = document.getElementsByClassName("rojo");
+    document.getElementsByClassName("amarillo")[0].style.backgroundColor = "rgb(255, 215, 0, 0.3)";
+  }
+
+  color = arreglo[inicial];
+
+  elemento[0].style.backgroundColor = color;
+}
+
+setInterval(() => {
+  cambiarColor(actual);
+}, 3000);
