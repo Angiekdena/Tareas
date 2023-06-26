a99b679d4a187a369b98da39223228fe2258eebb
Angie Cadena <ltejedor@outlook.com>
2023-06-26 15:49:54 -0500

    Adelanto cajero

--- Cajero/script.js
+++ Cajero/script.js
@@ -1,5 +1,36 @@
-function mostrarContraseña (){
-    // document.getElementById("password").hidden = false;
+let datos = [
+  { nombre: "Angie", password: "1234", saldo: 200 },
+  { nombre: "Luis", password: "5678", saldo: 290 },
+  { nombre: "Emma", password: "9034", saldo: 67 },
+];
 
-    document.querySelector("#password").hidden = false;
+let passwordDigitado = "";
+let cuentaSeleccionada = "";
+
+function escogerUsuario(usuario) {
+  let texto = document.getElementById("textoMostrar");
+  texto.innerText = `Bienvenido ${usuario}, por favor digita tu password`;
+  cuentaSeleccionada = usuario;
+}
+
+function recibePassword(numero) {
+  console.log(numero);
+  let password = document.getElementById("password");
+  password.innerText += `*`;
+  passwordDigitado += numero;
+  console.log(passwordDigitado);
+}
+
+function validarPassword() {
+  let usuario = datos.find((user) => user.nombre === cuentaSeleccionada);
+  
+  if(usuario){
+    let texto = document.getElementById("textoMostrar");
+
+    if(passwordDigitado === usuario.password){
+        texto.innerText = `Ingreso Exitoso`;
+    } else {
+        texto.innerText = `Password Incorrecto, digite nuevamente`;
+    }
+  }
 }
