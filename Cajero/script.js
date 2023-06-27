03edae5ec0000310de76a486d459a89b4102df1e
Angie Cadena <ltejedor@outlook.com>
2023-06-27 16:15:36 -0500

    Avance Cajero

--- Cajero/script.js
+++ Cajero/script.js
@@ -6,31 +6,130 @@ let datos = [
 
 let passwordDigitado = "";
 let cuentaSeleccionada = "";
+let opcionSeleccionada = 0;
 
 function escogerUsuario(usuario) {
-  let texto = document.getElementById("textoMostrar");
-  texto.innerText = `Bienvenido ${usuario}, por favor digita tu password`;
+  cambiarTextoPrincipal(
+    "textoMostrar",
+    `Bienvenido ${usuario}, por favor digita tu password`
+  );
   cuentaSeleccionada = usuario;
+  opcionSeleccionada = 1;
 }
 
-function recibePassword(numero) {
-  console.log(numero);
-  let password = document.getElementById("password");
-  password.innerText += `*`;
-  passwordDigitado += numero;
-  console.log(passwordDigitado);
+function recibeNumero(numero) {
+  if (opcionSeleccionada === 1) {
+    cambiarTextoPrincipal("password", "*", "si");
+    passwordDigitado += numero;
+  } else if (opcionSeleccionada === 3) {
+    cambiarTextoPrincipal("password", numero, "si");
+  } else if (opcionSeleccionada === 4) {
+    cambiarTextoPrincipal("password", numero, "si");
+  }
 }
 
-function validarPassword() {
+function enter() {
   let usuario = datos.find((user) => user.nombre === cuentaSeleccionada);
-  
-  if(usuario){
-    let texto = document.getElementById("textoMostrar");
 
-    if(passwordDigitado === usuario.password){
-        texto.innerText = `Ingreso Exitoso`;
+  if (opcionSeleccionada === 1) {
+    if (usuario) {
+      if (passwordDigitado === usuario.password) {
+        cambiarTextoPrincipal("textoMostrar", `Escoge una opcion:`);
+        cambiarTextoPrincipal("password", "");
+        document.getElementById("opciones").hidden = false;
+      } else {
+        cambiarTextoPrincipal(
+          "textoMostrar",
+          `Password Incorrecto, digite nuevamente:`
+        );
+        password.innerText = "";
+        passwordDigitado = "";
+      }
+    }
+  } else if (opcionSeleccionada === 3) {
+    let montoIngresado = parseInt(
+      document.getElementById("password").innerText
+    );
+    if (montoIngresado + usuario.saldo >= 990) {
+      cambiarTextoPrincipal(
+        "textoMostrar",
+        `No te podemos recibir $${montoIngresado}, porque supera el tope de cuenta de $990.`
+      );
+      cambiarTextoPrincipal("password", "");
     } else {
-        texto.innerText = `Password Incorrecto, digite nuevamente`;
+      usuario.saldo = montoIngresado + usuario.saldo;
+      cambiarTextoPrincipal(
+        "textoMostrar",
+        `El monto ingresaro fue de $${montoIngresado}, su saldo actualmente es de $${usuario.saldo}`
+      );
+      cambiarTextoPrincipal("password", "");
+    }
+  } else if (opcionSeleccionada === 4) {
+    let retirarMonto = parseInt(document.getElementById("password").innerText);
+    if (usuario.saldo - retirarMonto <= 10) {
+      cambiarTextoPrincipal(
+        "textoMostrar",
+        `No puedes retirar $${retirarMonto}, tope minimo de la cuenta debe ser de $10.`
+      );
+      cambiarTextoPrincipal("password", "");
+    } else {
+      usuario.saldo = usuario.saldo - retirarMonto;
+      cambiarTextoPrincipal(
+        "textoMostrar",
+        `El monto retirado fue de $${retirarMonto}, su saldo actualmente es de $${usuario.saldo}`
+      );
+      cambiarTextoPrincipal("password", "");
     }
   }
+}
+
+function escogerOpcion(seleccion) {
+  let usuario = datos.find((user) => user.nombre === cuentaSeleccionada);
+
+  console.log(escogerOpcion);
+  if (seleccion === 1) {
+    opcionSeleccionada = 2;
+    cambiarTextoPrincipal(
+      "textoMostrar",
+      `El saldo de su cuenta actualmente es de $${usuario.saldo}`
+    );
+    document.getElementById("opciones").hidden = true;
+  } else if (seleccion === 2) {
+    opcionSeleccionada = 3;
+    document.getElementById("opciones").hidden = true;
+    cambiarTextoPrincipal("textoMostrar", `Digite el monto a ingresar:`);
+  } else if (seleccion === 3) {
+    opcionSeleccionada = 4;
+    document.getElementById("opciones").hidden = true;
+    cambiarTextoPrincipal("textoMostrar", `Digite el monto a retirar:`);
+  }
+}
+
+function cambiarTextoPrincipal(elemento, textoRecibido, concatenar) {
+  let texto = document.getElementById(elemento);
+
+  if (concatenar) {
+    texto.innerText += textoRecibido;
+  } else {
+    texto.innerText = textoRecibido;
+  }
+}
+
+function volver() {
+  let usuario = datos.find((user) => user.nombre === cuentaSeleccionada);
+  if (usuario) {
+    if (passwordDigitado === usuario.password) {
+      cambiarTextoPrincipal("textoMostrar", `Escoge una opcion:`);
+      cambiarTextoPrincipal("password", "");
+      document.getElementById("opciones").hidden = false;
+    }
+  }
+}
+
+function cancelar() {
+  cambiarTextoPrincipal("textoMostrar", "Escoge un usuario");
+  passwordDigitado = "";
+  cuentaSeleccionada = "";
+  opcionSeleccionada = 0;
+  document.getElementById("opciones").hidden = true;
 }
